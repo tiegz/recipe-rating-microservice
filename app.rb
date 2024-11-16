b27ba3ff23817a7584b13bd7955cb6da0f2cc155
Tieg Zaharia <tieg.zaharia@gmail.com>
2024-11-15 21:08:38 -0700

    Adds POST /submit_rating

--- app.rb
+++ app.rb
@@ -15,4 +15,31 @@ set :bind, '0.0.0.0'
 FIXED_USER_ID = "565ebd10-2135-43ec-acc7-785f0d00fc8a"
 
 class RecipeRatingsApp < Sinatra::Base
+  # Example request: 
+  #   { "recipe_id": "111", "rating": 10, "optional_comment": ""}
+  post '/submit_rating' do
+    recipe_id, rating, optional_comment = *params.values_at(:recipe_id, :rating, :optional_comment)
+    rating = rating && rating.to_i
+    errors = []
+
+    errors << "'recipe_id' is missing" if recipe_id.nil?
+    errors << "'rating' is missing" if rating.nil?
+    errors << "'rating' is invalid (valid values: 1-10)" if rating && !(0..10).include?(rating)
+
+    recipe_rating = RecipeRating.find_or_initialize_by(
+      user_id: FIXED_USER_ID,
+      recipe_id: recipe_id
+    ) do |rr|
+      rr.id ||= SecureRandom.uuid
+    end
+    recipe_rating.update!(rating: rating, comment: optional_comment)
+
+    if errors.size > 0
+      status 422 # unprocessable entity
+      { errors: errors }.to_json
+    else
+      status 201 # created
+      recipe_rating.to_json
+    end
+  end
 end
